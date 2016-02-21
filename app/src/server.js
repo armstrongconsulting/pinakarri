b95ed0e052e78d539e5310d95b95d1555b99cadb
Christoph Schachinger <christoph@armstrongconsulting.net>
2016-02-21 22:21:13 +0100

    update

--- app/src/server.js
+++ app/src/server.js
@@ -14,6 +14,20 @@ const PORT = 8080;
 // App
 const app = express();
 
+var findUnit = function(req, res, callback){
+  mongo.connect(mongo_url, function(err, db) {
+          db.collection('units').findOne({uid:req.params.uid},function(err,unit){
+              if (unit != null){
+                callback(unit,db);
+              }else{
+                res.status(404).send('unit not found');
+              }
+          });
+     
+      });
+
+};
+
 app.use('/pinakarri', express.static(__dirname + '/public'));
 
 app.get('/pinakarri/units', function (req, res) {
@@ -26,28 +40,18 @@ app.get('/pinakarri/subscriptions', function (req, res) {
 });
 
 app.get('/pinakarri/api/unit/:uid', function (req, res) {
-
-    mongo.connect(mongo_url, function(err, db) {
-         var stream = db.collection('units').find().stream();
-      stream.on("data", function(item) {res.json(item)});
-      stream.on("end", function() {console.log('end')});
-
-
+    findUnit(req,res, function(unit){
+              res.json(unit);
     });
 
 });
 
 app.get('/pinakarri/api/unit/:uid/subscriptions', function (req, res) {
-  	var uid = req.params.uid
-
-  	res.json(
-  		[
-  			{ enabled: true, identifier:'OA01', name: 'Activity 1', external_link: 'http://www.google.at' , participants : { available: 20, booked : 2 }, leaders : { available: 4, booked : 0 } },
-  			{ enabled: true, identifier:'OA02', name: 'Activity 1', external_link: 'http://www.google.at' , participants : { available: 20, booked : 2 }, leaders : { available: 4, booked : 0 } },
-  			{ enabled: true, identifier:'OA03', name: 'Activity 1', external_link: 'http://www.google.at' , participants : { available: 20, booked : 2 }, leaders : { available: 4, booked : 0 } },
-  			{ enabled: true, identifier:'OA04', name: 'Activity 1', external_link: 'http://www.google.at' , participants : { available: 20, booked : 2 }, leaders : { available: 4, booked : 0 } },
-  			{ enabled: true, identifier:'OA05', name: 'Activity 1', external_link: 'http://www.google.at' , participants : { available: 20, booked : 2 }, leaders : { available: 4, booked : 0 } }
-  		]);
+    findUnit(req,res, function(unit,db){
+       db.collection('activities').find().toArray(function(err, docs){
+          res.json(docs);
+       });
+    });
 });
 
 app.post('/pinakarri/api/unit/:uid/subscription/:oa', function (req, res) {
